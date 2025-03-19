f67a84b1df5989ec914f058ccca3214e4584ab54
Jonathan Ariza <acamedawitch@gmail.com>
2025-03-18 21:41:41 -0500

    html y funcionalidad
    aun falta ajustar css pero esta activo y ejecutando

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# challenge-amigo-secreto_esp-main
+# <em>Challenge Amigo Secreto</em>
  actividd chllenge complementaria de allura
+
+
+## ¿Que es? ##
+juego interactivo de Amigo secreto, Actividad de formacion complementaria  desarollado por Allura Latam y realizado por Jonathan ariza como estudiante y aprendiz
+
+### Instrucciones ###
+ es sencillo 
+
+    - Agregar a la lista un numero de personas participantes del amigo secreto.
+    - Si el campo de escritura se encuentra vacío, entonces saltara una alerta de que se debe llenar el campo.
+    - Si no se ha introducido un ningún nombre a la lista tampoco se puede realizar el sorteo.
+    - Cada que se agregue un nombre a la lista, este se mostrara enumerado en pantalla.
+    - Si se busca limpiar la lista existe un botón para reiniciar el programa desde cero.
+
+## Agradecimientos y que lo disfruten ##
+
+ De momento eso es todo  que disfruten Este desarrolllo seguire actualizandolo y robusteciendolo futura mente.
+ 
--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombresSorteo = [];
+
+limpiarLista();
+
+function ingresarValor() {
+    return document.getElementById(`amigo`).value;
+}
+
+function agregarAmigo() {
+    var valorEnCaja = ingresarValor();
+    if(valorEnCaja == ``){
+        alert(`ingrese un nombre`);
+    }
+    else {
+        listaNombresSorteo.push(valorEnCaja);
+        document.getElementById(`amigo`).value=``;
+        document.getElementById(`amigo`).focus();
+    }
+    recorrerLista();
+}
+
+function recorrerLista(){
+    let listaAmigos = document.getElementById(`listaAmigos`)
+    listaAmigos.innerHTML = ``;
+
+    for(i=0; i<listaNombresSorteo.length; i++ )
+    {
+        let elementoHTML = document.createElement(`li`);
+        elementoHTML.textContent = `Num ${i+1} .- ${listaNombresSorteo[i]}`;
+        listaAmigos.appendChild(elementoHTML);
+    }
+}
+function sortearAmigo() {
+    if (listaNombresSorteo.length==0)
+    {
+        alert(`No hay participantes `);
+        return;
+    }
+    let amigoSecreto = listaNombresSorteo[Math.floor(Math.random()* listaNombresSorteo.length)];
+    let resultado = document.getElementById(`resultado`);
+    resultado.innerHTML = `Tu amigo secreto es: ${amigoSecreto}`;
+}
+function limpiarLista() {
+    listaNombresSorteo = [];
+    recorrerLista();
+    document.getElementById(`resultado`).innerHTML = ``;
+}
